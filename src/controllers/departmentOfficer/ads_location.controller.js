2b9e762596c4b9723100a67a05553ba2508e2ca4
minhchau1112 <tnmchau1211@gmail.com>
2024-01-11 11:01:38 +0700

    Updated BE for View Detail of function Management Ads location (Department)

--- src/controllers/departmentOfficer/ads_location.controller.js
+++ src/controllers/departmentOfficer/ads_location.controller.js
@@ -1,5 +1,6 @@
 import moment from "moment";
 import adsLocation from "../../services/departmentOfficer/ads_location.service.js";
+import multer from "multer";
 
 const index = async function (req, res) {
     const empty = false;
@@ -27,4 +28,55 @@ const index = async function (req, res) {
     });
 };
 
-export default { index };
+const addAdsLocation = async function(req, res){
+    const empty = false;
+    res.render("departmentOfficer/management_ads_location/add", {
+        empty: empty,
+    });
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, process.cwd() + '/static/images/ads-location/');
+    },
+    filename: function (req, file, cb) {
+        const filename = req.body.adsLocationId + '.' + file.originalname.split('.').pop();
+        req.body.image = '/static/images/ads-location/' + filename;
+        console.log(filename);
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+const uploadImage = async function(req, res) {
+    console.log("Đã vô upload");
+    
+    upload.single('image')(req, res, async function (err) {
+        if (err) {
+            console.error("error: ", err);
+            return res.status(500).json({ error: 'Error during upload.' });
+        } else {
+            console.log("file name: ", req.body.image);
+            const update = await Profile.updateUserInfo(userID, { image: req.body.image });
+            console.log("Đã up ảnh thành công, ", update);
+            return res.json({ success: true, image: req.body.image });
+        }
+    });
+}
+
+const handle_deleteAdsLocation = async function (req, res){
+    console.log("Body: ", req.body);
+    await districtService.del(req.body.adsLocationId);
+    res.redirect("departmentOfficer/management_ads_location/ads_location");
+}
+
+const viewDetailAdsLocation= async function (req, res){
+    const location = await adsLocation.findById(req.query.adsLocationId);
+    res.render("departmentOfficer/management_ads_location/view_detail", {
+        stt: req.query.stt,
+        adsLocation: location
+    });
+}
+
+export default { index, addAdsLocation, uploadImage, handle_deleteAdsLocation,  viewDetailAdsLocation };
--- src/services/departmentOfficer/ads_location.service.js
+++ src/services/departmentOfficer/ads_location.service.js
@@ -15,11 +15,24 @@ export default {
       .join('ward', 'ads_location.wardId', '=', 'ward.wardId').orderBy('ward_name');
   },
   findById(id) {
-    return db('ads_location').where('districtId', id).first();
+    return db('ads_location').select(
+      'ads_location.*',
+      'ads_type.name as ads_type_name',
+      'image.imgLink as image_link',
+      'location_type.name as location_type_name',
+      'ward.name as ward_name'
+    )
+    .join('ads_type', 'ads_location.adsType', '=', 'ads_type.adsTypeId')
+    .join('image', 'ads_location.imgId', '=', 'image.imgId')
+    .join('location_type', 'ads_location.locationType', '=', 'location_type.locationTypeId')
+    .join('ward', 'ads_location.wardId', '=', 'ward.wardId').where('adsLocationId', id).first();
+  },
+  del(id) {
+    return db('ads_location').where('adsLocationId', id).del();
   },
   patch(entity) {
     const id = entity.districtId;
     delete entity.districtId;
     return db('district').where('districtId', id).update(entity);
-  }
+  },
 }
