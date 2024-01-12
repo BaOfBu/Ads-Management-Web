e9f2d3d78053e49fb13698b907586189d0715ebe
minhchau1112 <tnmchau1211@gmail.com>
2024-01-12 15:33:08 +0700

    Updated BE for View list of function Management Ads Panel (Department)

--- src/controllers/departmentOfficer/ads_panel.controller.js
+++ src/controllers/departmentOfficer/ads_panel.controller.js
@@ -1,7 +1,29 @@
+import moment from "moment";
+import adsPanel from "../../services/departmentOfficer/ads_panel.service.js";
+
 const index = async function (req, res) {
-    const empty = true;
+    const empty = false;
+    const ads_panels = await adsPanel.findAll();
+
+    console.log("List ads_panels: ", ads_panels);
+
+    if(!ads_panels || ads_panels.length === 0){
+        empty = true;
+    }
+
+    const ads_panelsWithIndex = ads_panels.map((ads_panel, index) => ({
+        ...ads_panel,
+        stt: index + 1,
+    }));
+
+    console.log("List có index: ", ads_panelsWithIndex);
+
+    const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY');
+
     res.render("departmentOfficer/management_ads_panel/ads_panel", {
-        empty: empty
+        empty: empty,
+        ads_panels: ads_panelsWithIndex,
+        date: currentDateTime
     });
 };
 
---
+++ src/services/departmentOfficer/ads_panel.service.js
@@ -0,0 +1,15 @@
+import db from '../../utils/db.js';
+
+export default {
+  findAll() {
+    return db('ads_panel').select(
+        'ads_panel.*',
+        'image.imgLink as image_link',
+        'ads_location.location as ads_location_name',
+        'ads_panel_type.name as ads_panel_type_name'
+      )
+      .join('image', 'ads_panel.imgId', '=', 'image.imgId')
+      .join('ads_location', 'ads_panel.adsLocationId', '=', 'ads_location.adsLocationId')
+      .join('ads_panel_type', 'ads_panel.adsPanelTypeId', '=', 'ads_panel_type.adsPanelTypeId');
+  }
+}
