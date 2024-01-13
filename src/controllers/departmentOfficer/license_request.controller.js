82a07fb6e71243bc2f383e5a66df629b5f9e9a05
minhchau1112 <tnmchau1211@gmail.com>
2024-01-14 00:48:41 +0700

    Updated BE for View list of function License Request (Department)

--- src/controllers/departmentOfficer/license_request.controller.js
+++ src/controllers/departmentOfficer/license_request.controller.js
@@ -1,8 +1,29 @@
+import moment from "moment";
+import licenseRequest from "../../services/departmentOfficer/license_request.service.js";
+import district from "../../services/departmentOfficer/district.service.js";
+
 const index = async function (req, res) {
     let empty = false;
+    const license_request = await licenseRequest.findAll();
+    const districts = await district.findAll();
+    if(!license_request || license_request.length === 0){
+        empty = true;
+    }
+
+    let license_requestWithIndex = license_request.map((request, index) => ({
+        ...request,
+        startDate: moment(request.startDate).format('DD/MM/YYYY'),
+        endDate: moment(request.endDate).format('DD/MM/YYYY'),
+        stt: index + 1,
+    }));
+
+    const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY');
 
     res.render("departmentOfficer/license_request/list", {
         empty: empty,
+        license_request: license_requestWithIndex,
+        districts: districts,
+        date: currentDateTime
     });
 };
 
---
+++ src/services/departmentOfficer/license_request.service.js
@@ -0,0 +1,27 @@
+import db from '../../utils/db.js';
+
+export default {
+  findAll() {
+    return db('license_request').select(
+        'license_request.*',
+        'ads_location.location as ads_location_name',
+        'location_type.name as ads_location_type_name',
+        'ads_panel_type.name as ads_panel_type_name',
+        'ads_type.name as ads_type_name',
+        'ads_panel.width as width',
+        'ads_panel.height as height',
+        'ward.name as ward_name',
+        'district.name as district_name',
+        'image.imgLink as img_link'
+      )
+      .leftJoin('ads_location', 'license_request.adsLocationId', 'ads_location.adsLocationId')
+      .leftJoin('location_type', 'ads_location.locationType', 'location_type.locationTypeId')
+      .leftJoin('ads_type', 'ads_location.adsType', 'ads_type.adsTypeId')
+      .leftJoin('ads_panel', 'license_request.adsPanelId', 'ads_panel.adsPanelId')
+      .leftJoin('ads_panel_type', 'ads_panel.adsPanelTypeId', 'ads_panel_type.adsPanelTypeId')
+      .leftJoin('ward', 'license_request.wardId', 'ward.wardId')
+      .leftJoin('district', 'license_request.districtId', 'district.districtId')
+      .leftJoin('image', 'license_request.imgId', 'image.imgId')
+      .orderBy('startDate');
+  },
+}
