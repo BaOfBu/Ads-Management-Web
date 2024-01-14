fc42a6bcd7c6a823dd2e3fbd9a68e7609b195a6a
minhchau1112 <tnmchau1211@gmail.com>
2024-01-14 12:06:45 +0700

    Updated BE for View list of function License Request (Ward Officer)

--- src/controllers/wardOfficer/license_request.controller.js
+++ src/controllers/wardOfficer/license_request.controller.js
@@ -1,8 +1,28 @@
+import moment from "moment";
+import licenseRequest from "../../services/departmentOfficer/license_request.service.js";
+
 const index = async function (req, res) {
+    const user = req.session.authUser;
     let empty = false;
+    const license_request = await licenseRequest.findByWardId(user.wardId);
+
+    if(!license_request || license_request.length === 0){
+        empty = true;
+    }
+
+    let license_requestWithIndex = license_request.map((request, index) => ({
+        ...request,
+        startDate: moment(request.startDate).format('DD/MM/YYYY'),
+        endDate: moment(request.endDate).format('DD/MM/YYYY'),
+        stt: index + 1,
+    }));
+
+    const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY');
 
     res.render("wardOfficer/license_request", {
         empty: empty,
+        license_request: license_requestWithIndex,
+        date: currentDateTime
     });
 };
 
