4810c80eac7f42e3157fe583c15d04966f306986
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-01-17 14:44:31 +0700

    xem danh sách điểm đặt, bảng qc của CB quận

---
+++ src/controllers/districtOfficer/ads.controller.js
@@ -0,0 +1,115 @@
+import adsService from "../../services/wardOfficer/ads.service.js";
+import moment from "moment";
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+import { get } from "http";
+
+const statusName = ["Đã quy hoạch","Chưa quy hoạch"];
+
+const index = async function (req, res) {
+    const user = req.session.authUser;
+    //console.log("user",user);
+    const wardName = await adsService.findWardByWardId(user.wardId);
+    const districtName = await adsService.findDistrictByDistrictId(user.districtId);
+    const arrayAdsLocation = await adsService.findAllAdsLocationByWardId(user.wardId);
+    // console.log("wardName",wardName);
+    // console.log("districtName",districtName);
+    //console.log("arrayAdsLocation",arrayAdsLocation);
+    const adsLocationWithIndex = arrayAdsLocation.map((adsLocation, index) => ({
+        ...adsLocation,
+        stt: index + 1
+    }));
+    const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY')
+    return res.render("wardOfficer/ads_location", {
+        wardName: wardName.name,
+        districtName: districtName.name,
+        arrayAdsLocation: adsLocationWithIndex,
+        date: currentDateTime,
+        isEmpty: adsLocationWithIndex.length == 0
+    });
+}
+
+const postAdsLocation = async function (req, res) {
+    console.log("req.body:", req.body);
+    const keyword = req.body.keyword || "";
+    const user = req.session.authUser;
+    //console.log("user",user);
+    const wardName = await adsService.findWardByWardId(user.wardId);
+    const districtName = await adsService.findDistrictByDistrictId(user.districtId);
+    const arrayAdsLocation = await adsService.findAllAdsLocationByKeyword(user.wardId,keyword);
+    // console.log("wardName",wardName);
+    // console.log("districtName",districtName);
+    //console.log("arrayAdsLocation",arrayAdsLocation);
+    const adsLocationWithIndex = arrayAdsLocation.map((adsLocation, index) => ({
+        ...adsLocation,
+        stt: index + 1
+    }));
+    const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY')
+    return res.render("wardOfficer/ads_location", {
+        wardName: wardName.name,
+        districtName: districtName.name,
+        arrayAdsLocation: adsLocationWithIndex,
+        date: currentDateTime,
+        isEmpty: adsLocationWithIndex.length == 0
+    });
+    res.render("wardOfficer/ads_location");
+}
+
+const viewDetails = async function (req, res) {
+    const ads_panel = await adsService.findAllAdsPanelByAdsLocationId(req.query.adsLocationId);
+    const adsLocationName = await adsService.findAdsLocationName(req.query.adsLocationId);
+    //console.log("ads_panel",ads_panel);
+    //console.log("adsLocationName",adsLocationName);
+    const adsPanelWithIndex = ads_panel.map((adsPanel, index) => ({
+        ...adsPanel,
+        stt: index + 1
+    }));
+    //console.log("adsPanelWithIndex",adsPanelWithIndex);
+    //console.log("Length",adsPanelWithIndex.length==0);
+    const isEmpty = adsPanelWithIndex.length == 0;
+    //console.log("isEmpty",isEmpty);
+    res.render("wardOfficer/ads_panel", {
+        adsLocationName: adsLocationName.location,
+        adsPanel: adsPanelWithIndex,
+        isEmpty: isEmpty
+    })
+};
+
+const viewPanelDetails = async function (req, res) {
+    //console.log("req.query.adsPanelId",req.query.adsPanelId);
+    const ads_panel = await adsService.findAdsPanel(req.query.adsPanelId);
+    //console.log("ads_panel nè",ads_panel);
+    res.render("wardOfficer/ads_panel_detail", {
+        adsPanel: ads_panel
+    })
+}
+
+const getEditAdsLocation = async function (req, res) {
+    //console.log("req.query",req.query);
+    const adsLocation = await adsService.findAdsLocation(req.query.adsLocationId);
+    //console.log("adsLocation nè",adsLocation);
+    const LocationType = await adsService.findAllLocationTypeName();
+    const adsType = await adsService.findAllAdsTypeName();
+    //console.log("adsLocation",adsLocation);
+
+    res.render("wardOfficer/edit_ads_location", {
+        status: statusName,
+        adsLocation: adsLocation,
+        locationType: LocationType,
+        adsType: adsType
+    })
+}
+const getEditAdsPanel = async function (req, res) {
+    const adsPanelType = await adsService.findAllAdsPanelType();
+    const adsPanel = await adsService.findAdsPanel(req.query.adsPanelId);
+    //console.log("adsPanel",adsPanel);
+    //console.log("adsPanelType",adsPanelType);
+    res.render("wardOfficer/edit_ads_panel", {
+        adsPanelType : adsPanelType,
+        adsPanel : adsPanel
+    })
+}
+
+
+export default { index, viewDetails, viewPanelDetails, getEditAdsLocation, getEditAdsPanel, postAdsLocation};
---
+++ src/services/districtOfficer/ads.route.js
@@ -0,0 +1,89 @@
+import express from "express";
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+import adsController from "../../controllers/districtOfficer/ads.controller.js";
+import adsService from "../../services/districtOfficer/ads.service.js";
+const router = express.Router();
+
+const uploadAdsLocationRequest = "static/images/ads-location/";
+if (!fs.existsSync(uploadAdsLocationRequest)) {
+    fs.mkdirSync(uploadAdsLocationRequest, { recursive: true });
+}
+function getNextFilename(folderPath) {
+    const files = fs.readdirSync(folderPath);
+    const maxIndex = files.reduce((max, file) => {
+        const currentIndex = parseInt(path.basename(file, path.extname(file)), 10);
+        return currentIndex > max ? currentIndex : max;
+    }, -1);
+
+    return (maxIndex + 1).toString();
+}
+const storageAdsLocation = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadAdsLocationRequest);
+    },
+    filename: function (req, file, cb) {
+        const filename = getNextFilename(uploadAdsLocationRequest) + path.extname(file.originalname);
+        console.log(filename);
+        cb(null, filename);
+    }
+});
+
+const uploadAdsLocation = multer({ storage: storageAdsLocation });
+
+router.get("/", adsController.index);
+router.post("/", adsController.postAdsLocation);
+router.get("/view-detail", adsController.viewDetails);
+router.get("/view-panel-detail", adsController.viewPanelDetails);
+router.get("/ads-location-edit", adsController.getEditAdsLocation);
+router.get("/ads-panel-edit", adsController.getEditAdsPanel);
+router.post("/ads-location-edit", uploadAdsLocation.array("image"),async function(req,res,next){
+    //console.log("req.body:", req.body);
+    const filePath = req.files[0].path;
+    let fileName = "/" + filePath.replace(/\\/g, "/"); 
+    const imgId = await adsService.saveImage(fileName);
+    const adsLocation = await adsService.findAdsLocationRaw(req.body.txtAdsLocationId);
+    const ward = await adsService.findWardByWardId(adsLocation.wardId);
+    const entity = {
+        AdsLocationId: req.body.txtAdsLocationId,
+        AdsLocationTypeId : req.body.LocationType,
+        AdsTypeId : req.body.AdsType,
+        ImgId : imgId,
+        RequestReason : req.body.RequestReason,
+        WardId : adsLocation.wardId,
+        status:"Chưa duyệt",
+        RequestTime: new Date(),
+        districtId: ward.districtId,
+    }
+    const ads_location_edit = await adsService.createAdsLocationEdit(entity);
+    //const update = await adsService.updateMerchantInfo(userID, { image: req.body.image });
+    //return res.json({ success: true, image: req.body.image });
+    return res.redirect("/ward-officer/ads");
+});
+
+router.post("/ads-panel-edit", async function(req,res,next){
+    console.log("req.body:", req.body);
+    const adsLocation = await adsService.findAdsLocationRaw(req.body.txtAdsLocationId);
+    const ward = await adsService.findWardByWardId(adsLocation.wardId);
+    const entity = {
+        AdsPanelId: req.body.txtAdsPanelId,
+        AdsLocationId: req.body.txtAdsLocationId,
+        AdsPanelTypeId : req.body.adsPanelType,
+        RequestReason : req.body.RequestReason,
+        WardId : adsLocation.wardId,
+        status:"Chưa duyệt",
+        RequestTime: new Date(),
+        districtId: ward.districtId,
+        Height: req.body.Height,
+        Width: req.body.Width,
+        Quantity: req.body.Quantity,
+
+    }
+    const ads_panel_edit = await adsService.createAdsPanelEdit(entity);
+    //const update = await adsService.updateMerchantInfo(userID, { image: req.body.image });
+    //return res.json({ success: true, image: req.body.image });
+    return res.redirect("/ward-officer/ads/");
+});
+
+export default router;
