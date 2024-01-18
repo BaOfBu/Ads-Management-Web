0f19ae954305a5e1ed7d0943d8379efa7415000d
minhchau1112 <tnmchau1211@gmail.com>
2024-01-18 09:54:24 +0700

    Updated BE for view list by filter of function Ads Location Modification Request (Department)

--- src/controllers/departmentOfficer/ads_location_modification_request.controller.js
+++ src/controllers/departmentOfficer/ads_location_modification_request.controller.js
@@ -9,27 +9,158 @@ import adsLocation from "../../services/departmentOfficer/ads_location.service.j
 const index = async function (req, res) {
     let empty = false;
 
-    const edit_ads_locations_request = await adsLocationModificationRequest.findAll();
     const districts = await district.findAll();
+    const districtCurrent = req.query.districtId || -1;
+    const wardCurrent = req.query.wardId || -1;
+    const page = req.query.page || 1;
+    const wards = await ward.findAllByDistrictId(districtCurrent);
+    let districtName = "Tất cả quận";
+    let wardName = "Tất cả phường";
+
+    const edit_ads_locations_request = await getListByWard(wardCurrent, districtCurrent);
+
+    if (districtCurrent !== -1 && districtCurrent !== "-1"){
+        const tmpDistrict = await providedInfo.findById('district', 'districtId', districtCurrent);
+        districtName = "Quận " + tmpDistrict.name;
+    } 
+    if (wardCurrent !== -1 && wardCurrent !== "-1"){
+        const tmpWard = await providedInfo.findById('ward', 'wardId', wardCurrent);
+        wardName = "Phường " + tmpWard.name;
+    } 
+
     if(!edit_ads_locations_request || edit_ads_locations_request.length === 0){
         empty = true;
     }
 
-    let edit_ads_locations_requestWithIndex = edit_ads_locations_request.map((request, index) => ({
-        ...request,
-        requestTime: moment(request.requestTime).format('DD/MM/YYYY'),
-        stt: index + 1,
-    }));
-
     const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY');
+
+    const pagination = generatePagination(edit_ads_locations_request, districtCurrent, wardCurrent, page);
 
     res.render("departmentOfficer/ads_location_modification_request/list", {
         empty: empty,
-        edit_ads_locations_request: edit_ads_locations_requestWithIndex,
+        edit_ads_locations_request: pagination.list,
         districts: districts,
-        date: currentDateTime
+        date: currentDateTime,
+        wards: wards,
+        isFirstPage: pagination.isFirstPage,
+        isLastPage: pagination.isLastPage,
+        pageNumbers: pagination.pageNumbers,
+        page: page,
+        districtId: districtCurrent,
+        wardId: wardCurrent,
+        districtName: districtName,
+        wardName: wardName
     });
 };
+
+function generatePagination(request, districtId, wardId, pageCurrent){
+    const limit = 8;
+    const page = pageCurrent;
+    const offset = (page - 1) * limit;
+
+    const total = request.length;
+    const nPages = Math.ceil(total / limit);
+
+    let pageNumbers = [];
+    if(nPages <= 7){
+        for (let i = 1; i <= nPages; i++) {
+            pageNumbers.push({
+                value: i,
+                isActive: i === +page,
+                districtId: districtId,
+                wardId: wardId
+            });
+        }
+    }else{
+        if(Number(page) + 2 <= nPages){
+            if(Number(page) > 5){
+                for (let i = 1; i <= 2; i++) {
+                    pageNumbers.push({
+                        value: i,
+                        isActive: i === +page,
+                        districtId: districtId,
+                        wardId: wardId
+                    });
+                }
+                pageNumbers.push({
+                    value: '..',
+                    isActive: false,
+                    districtId: districtId,
+                    wardId: wardId
+                });
+                for (let i = Number(page) - 2; i <= Number(page) + 2; i++) {
+                    pageNumbers.push({
+                        value: i,
+                        isActive: i === +page,
+                        districtId: districtId,
+                        wardId: wardId
+                    });
+                }  
+            }else if(Number(page) > 3){
+                for (let i = Number(page) - 3; i <= Number(page) + 3; i++) {
+                    pageNumbers.push({
+                        value: i,
+                        isActive: i === +page,
+                        districtId: districtId,
+                        wardId: wardId
+                    });
+                }    
+            }else{
+                for (let i = 1; i <= 7; i++) {
+                    pageNumbers.push({
+                        value: i,
+                        isActive: i === +page,
+                        districtId: districtId,
+                        wardId: wardId
+                    });
+                } 
+            }
+        }else if(Number(page) + 2 > nPages){
+            for (let i = 1; i <= 2; i++) {
+                pageNumbers.push({
+                    value: i,
+                    isActive: i === +page,
+                    districtId: districtId,
+                    wardId: wardId
+                });
+            }
+            pageNumbers.push({
+                value: '..',
+                isActive: false,
+                districtId: districtId,
+                wardId: wardId
+            });
+            for (let i = nPages - 4; i <= nPages; i++) {
+                pageNumbers.push({
+                    value: i,
+                    isActive: i === +page,
+                    districtId: districtId,
+                    wardId: wardId
+                });
+            }
+        }    
+    }
+
+    let list = request;
+    if(total > offset){
+        list = request.slice(offset, offset+limit); 
+    }
+
+    let isFirstPage = false;
+    if(Number(page) === 1) isFirstPage = true;
+
+    let isLastPage = false;
+    if(Number(page) === nPages || nPages === 0) isLastPage = true;
+
+    const pagination = {
+        list: list,
+        pageNumbers: pageNumbers,
+        isFirstPage: isFirstPage,
+        isLastPage: isLastPage
+    };
+
+    return pagination;
+}
 
 const getWardByDistrict = async function(req, res){
     const wards = await ward.findAllByDistrictId(req.body.districtId);
@@ -37,18 +168,28 @@ const getWardByDistrict = async function(req, res){
     return res.json({success: true, wards: wards});
 }
 
-const getRequestByWard = async function(req, res){
-    const requests = await adsLocationModificationRequest.findByWardId(req.body.wardId);
+async function getListByWard(wardId, districtId){
+    let requests;
+    console.log("districtId: ", districtId);
+    console.log("wardId: ", wardId);
+    if(wardId === -1 || wardId === "-1"){
+        if(districtId === -1 || districtId === "-1"){
+            console.log("Đã vô đây");
+            requests = await adsLocationModificationRequest.findAll();   
+        }else{
+            requests = await adsLocationModificationRequest.findAllByDistrict(districtId);
+        }  
+    }else{
+        requests = await adsLocationModificationRequest.findByWardId(wardId);
+    }
 
-    let request = requests.map((request, index) => ({
+    let list = requests.map((request, index) => ({
         ...request,
-        requestTime: moment(request.requestTime).format('DD/MM/YYYY HH:mm:ss'),
+        requestTime: moment(request.requestTime).format("DD/MM/YYYY"),
         stt: index + 1,
     }));
 
-    const currentDateTime = moment().format('HH:mm:ss DD-MM-YYYY');
-    console.log("request: ", request);
-    return res.json({success: true, request: request, date: currentDateTime});   
+    return list;
 }
 
 const cancelRequest = async function(req, res){
@@ -98,4 +239,4 @@ const viewDetail = async function(req, res){
     });
 }
 
-export default { index, getWardByDistrict, getRequestByWard, cancelRequest, acceptRequest, viewDetail };
+export default { index, getWardByDistrict, cancelRequest, acceptRequest, viewDetail };
--- src/routes/departmentOfficer/ads_location_modification_request.route.js
+++ src/routes/departmentOfficer/ads_location_modification_request.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", adsLocationModificationRequestController.index);
 router.post("/get-ward-by-district", adsLocationModificationRequestController.getWardByDistrict);
-router.post("/get-request-by-ward", adsLocationModificationRequestController.getRequestByWard);
+// router.post("/get-request-by-ward", adsLocationModificationRequestController.getRequestByWard);
 router.post("/cancel-request", adsLocationModificationRequestController.cancelRequest);
 router.post("/accept-request", adsLocationModificationRequestController.acceptRequest);
 router.get("/view-detail", adsLocationModificationRequestController.viewDetail);
